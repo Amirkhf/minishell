6101dc8a29968143325fa98b861640f06f815cb2
Amir Khelifi <amkhelif@f4r13s10.clusters.42paris.fr>
2026-03-02 10:22:16 +0100

    ok

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,21 @@
 /*   By: amkhelif <amkhelif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 08:27:35 by amkhelif          #+#    #+#             */
-/*   Updated: 2026/02/26 16:24:58 by amkhelif         ###   ########.fr       */
+/*   Updated: 2026/02/26 17:20:38 by amkhelif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void	loop(t_data *data)
+{
+	while (1)
+	{
+		data->line = readline("test : ");
+		gc_add(&data->lst_free, data->line, STRING);
+		add_history(data->line);
+	}
+}
 
 int	main(int argc, char **argv, char **env)
 {
@@ -19,14 +29,6 @@ int	main(int argc, char **argv, char **env)
 
 	i = 0;
 	init_struct(&data, env);
-	while (1)
-	{
-		if (i == 3)
-			break ;
-		data.line = readline("test : ");
-		gc_add(&data.lst_free, data.line, STRING);
-		add_history(data.line);
-		i++;
-	}
+	loop(&data);
 	return (free_all(data.lst_free), EXIT_SUCCESS);
 }
