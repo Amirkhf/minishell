c833dece77fed96fb2372482663710975f2346bf
iYamox <alexandre.mary78360@gmail.com>
2026-03-25 12:13:33 +0100

    minishell

--- srcs/exec/signals.c
+++ srcs/exec/signals.c
@@ -6,7 +6,7 @@
 /*   By: amary <amary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 12:39:45 by amary             #+#    #+#             */
-/*   Updated: 2026/03/24 15:43:44 by amary            ###   ########.fr       */
+/*   Updated: 2026/03/24 15:45:08 by amary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,23 @@
 #include <signal.h>
 #include <readline/readline.h>
 
+// 1. La fonction qui réagit quand on appuie sur Ctrl+C
 void	handle_sigint(int sig)
 {
-	(void)sig;
-	
-	write(1, "\n", 1);
-	
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
+	(void)sig; // On ne se sert pas du numéro du signal
+	write(1, "\n", 1);	
+	// Magie de Readline pour réafficher le prompt proprement
+	rl_on_new_line();      // Dit à readline qu'on est sur une nouvelle ligne
+	rl_replace_line("", 0); // Efface ce que l'utilisateur était en train d'écrire
+	rl_redisplay();        // Réaffiche "Minishell : "
 }
 
+// 2. La fonction pour brancher nos signaux
 void	setup_signals(void)
 {
+	// Quand le système reçoit SIGINT (Ctrl+C), il appelle handle_sigint
 	signal(SIGINT, handle_sigint);
+	
+	// Quand le système reçoit SIGQUIT (Ctrl+\), on lui dit de l'ignorer (SIG_IGN)
 	signal(SIGQUIT, SIG_IGN);
 }
