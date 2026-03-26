c8457b53f46d6bc54ddd53f6c7139e1a82b4f4a4
amkhelif <amkhelif@f1br6s1.paris.42.school>
2026-03-26 12:57:02 +0100

    ok

---
+++ srcs/exec/built-ins/unset.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amkhelif <amkhelif@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/26 12:02:52 by amkhelif          #+#    #+#             */
+/*   Updated: 2026/03/26 12:02:53 by amkhelif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../includes/minishell.h"
+
+
+static int	is_valid_name(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str == NULL || str[0] == '\0' || (str[0] >= '0' && str[0] <= '9'))
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isalpha(str[i]) && str[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static void	remove_env_var(t_data *data, int index)
+{
+	while (data->env[index])
+	{
+		data->env[index] = data->env[index + 1];
+		index++;
+	}
+}
+
+void	ft_unset(t_data *data, t_cmd *cmd)
+{
+	int	i;
+	int	j;
+	int	len;
+
+	i = 1;
+	while (cmd->args[i])
+	{
+		if (!is_valid_name(cmd->args[i]))
+			break;
+		else
+		{
+			j = 0;
+			len = ft_strlen(cmd->args[i]);
+			while (data->env[j])
+			{
+				if (ft_strncmp(data->env[j], cmd->args[i], len) == 0
+					&& (data->env[j][len] == '=' || data->env[j][len] == '\0'))
+				{
+					remove_env_var(data, j);
+					break ;
+				}
+				j++;
+			}
+		}
+		i++;
+	}
+}
